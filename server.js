9109a2994c99f2c6d272bfc0f98daf3ca3aa7672
creativej <lu.jamison@gmail.com>
2014-02-22 14:35:59 +1100

    Renamed the server

--- server.js
+++ server.js
@@ -1,10 +1,51 @@
 var
     dbConfig = require('./config/database.json'),
-    server = {};
+    tables = require('./config/tables'),
+    cronJob = require('cron').CronJob,
+    en = require('lingo').en,
+    server = {
+        mode: process.env.mode || 'dev',
+        config: function(name) {
+            return {
+                db: dbConfig[this.mode]
+            }[name];
+        }
+    },
+    orm = require('./config/orm')(server.config('db'))
+    ;
 
-server.mode = process.env.mode || 'dev';
-server.config = {
-    db: dbConfig[server.mode]
+server.init = function() {
+    for (var tableName in tables) {
+        var table = tables[tableName];
+
+        new cronJob(table.rate, job(tableName, table), null, true);
+    }
+
+    function job(tableName, table) {
+        return function() {
+            var random = Math.random();
+
+            if (typeof table.randomOffset === 'undefined' || random <= table.randomOffset) {
+                console.log('insert record to', tableName);
+
+                var data = {};
+
+                for (var field in table.fields) {
+                    var value = table.fields[field]();
+                    data[field] = value;
+
+                    console.log(field, '-', value);
+                }
+
+                var modelName = en.singularize(tableName);
+
+                orm.models[modelName].create(data).success(function() {
+                    console.log('... done!');
+                });
+            }
+        };
+    }
+
 };
 
-module.exports = server;
+server.init();
